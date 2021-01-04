3c764aa5d2b9651033c6705b6c48fdbf3d0b6817
Thomas <tommyprezioso@gmail.com>
2021-01-04 17:17:32 -0500

    Initial Commit

---
+++ DSNYPickup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DSNYPickup
+//
+//  Created by Thomas Prezioso Jr on 1/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
