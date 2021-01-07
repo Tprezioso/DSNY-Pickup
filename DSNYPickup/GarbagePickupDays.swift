f1e0c62f72a6a5505575d0652e6d366243340086
Thomas <tommyprezioso@gmail.com>
2021-01-06 21:43:32 -0500

    planning stage

--- DSNYPickup/GarbagePickupDays.swift
+++ DSNYPickup/GarbagePickupDays.swift
@@ -7,15 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GarbagePickupDays: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("SearchView")
+                .padding()
+            Text("InfoView")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        GarbagePickupDays()
     }
 }
