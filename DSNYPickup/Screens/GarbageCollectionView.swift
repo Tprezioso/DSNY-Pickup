17e7a633c10e1fff32c67a36dd6d6c89234e76ee
Thomas <tommyprezioso@gmail.com>
2023-01-15 23:12:08 -0500

    added dismissing keyboard on tap of screen

--- DSNYPickup/Screens/GarbageCollectionView.swift
+++ DSNYPickup/Screens/GarbageCollectionView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct GarbageCollectionView: View {
     let columns = Array(repeating: GridItem(.flexible()), count: 6)
     @StateObject var viewModel = GarbageCollectionStateModel()
+    init() {
+        UILabel.appearance(whenContainedInInstancesOf: [UINavigationBar.self]).adjustsFontSizeToFitWidth = true
+    }
     
     var body: some View {
         
@@ -17,33 +20,38 @@ struct GarbageCollectionView: View {
                 NavigationStack {
                     ScrollView {
                         VStack {
-                            HStack {
-                                TextField("Address", text: $viewModel.textString, prompt: Text("When is collecting at..."))
-                                    .textFieldStyle(.roundedBorder)
-                                    .submitLabel(.search)
-                                    .onSubmit {
-                                        viewModel.getGarbageCollectionData()
-                                    }
-                            }.padding()
-                            
+                            VStack(alignment: .leading) {
+                                            HStack {
+                                                Image(systemName: "magnifyingglass")
+                                                TextField("Search...", text: $viewModel.textString, prompt: Text("When is collecting at..."))
+                                            }
+                                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                                            .submitLabel(.search)
+                                            .onSubmit {
+                                                viewModel.getGarbageCollectionData()
+                                            }
+                                        }.padding()
+
                             GarbageCollectionGridView(
-                                garbage: viewModel.garbage,
-                                largeItems: viewModel.largeItems,
-                                recycling: viewModel.recycling,
-                                composting: viewModel.composting
-                            )
+                                    garbage: viewModel.garbage,
+                                    largeItems: viewModel.largeItems,
+                                    recycling: viewModel.recycling,
+                                    composting: viewModel.composting
+                                )
                             Spacer()
-                            
                         }.onAppear {
-                            
+                            viewModel.getGarbageCollectionData()
                         }
-                    }.navigationTitle("Collection Schedule")
+                    }.navigationTitle("DSNY Garbage Collection")
                 }
             if viewModel.isLoading {
                 ProgressView()
             }
         } .alert(item: $viewModel.alertItem) { alertItem in
             Alert.init(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+        }
+        .onTapGesture {
+            dismissKeyboardOnTap()
         }
     }
 }
@@ -84,4 +92,11 @@ enum EnumDays: Int, CustomStringConvertible
         }
     }
     case MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
+}
+
+extension View {
+  func dismissKeyboardOnTap() {
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                    to: nil, from: nil, for: nil)
+  }
 }
