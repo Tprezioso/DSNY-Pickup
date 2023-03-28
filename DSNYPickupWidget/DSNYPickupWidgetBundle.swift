71eb9bb7e00b701a2cfa052e76d8a67aa4fbfe01
Thomas <tommyprezioso@gmail.com>
2023-03-28 19:52:36 -0400

    adding widget target

---
+++ DSNYPickupWidget/DSNYPickupWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DSNYPickupWidgetBundle.swift
+//  DSNYPickupWidget
+//
+//  Created by Thomas Prezioso Jr on 3/28/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DSNYPickupWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DSNYPickupWidget()
+    }
+}
